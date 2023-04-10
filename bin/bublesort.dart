771cf5527ae3f43c1a29c5c2e7771c41c297f6f8
Deriks Karlay Dias Costa <karlaycosta@gmail.com>
2023-04-10 15:00:35 -0300

    first commit

---
+++ bin/bublesort.dart
@@ -0,0 +1,11 @@
+void bubleSort(List<int> lista) {
+  for (var i = 0; i < lista.length; i++) {
+    for (var ii = 0; ii < lista.length - 1; ii++) {
+      if (lista[ii] > lista[ii + 1]) {
+        final tmp = lista[ii];
+        lista[ii] = lista[ii + 1];
+        lista[ii + 1] = tmp;
+      }
+    }
+  } 
+}
---
+++ bin/quicksort.dart
@@ -0,0 +1,16 @@
+List<int> quickSort(List<int> lista) {
+  if (lista.length < 2) {
+    return lista;
+  }
+  final esquerda = <int>[];
+  final direita = <int>[];
+  final pivo = lista[0];
+  for (var i = 1; i < lista.length; i++) {
+    if (lista[i] < pivo) {
+      esquerda.add(lista[i]);
+    } else {
+      direita.add(lista[i]);
+    }
+  }
+  return [...quickSort(esquerda), pivo, ...quickSort(direita)];
+}
